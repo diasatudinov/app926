fecd131ca4780efe5fe0f899d63db3eacbabf3ff
Dias <dias.atudinov@gmail.com>
2024-10-02 15:54:52 +0500

    Loader and ReOnboarding UIView

--- app926/app926/First/Onboarding/ReOnboardingUIView.swift
+++ app926/app926/First/Onboarding/ReOnboardingUIView.swift
@@ -8,10 +8,141 @@
 import SwiftUI
 
 struct ReOnboardingUIView: View {
+    @State private var pageNum: Int = 1
+    @State private var showSheet = false
+    @AppStorage("signedUP") var signedUP: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        if !signedUP {
+            ZStack {
+                Color.loaderBG
+//                LinearGradient(gradient: Gradient(colors: [.gradientTop, .gradientBottom]),
+//                               startPoint: .top,
+//                               endPoint: .bottom)
+                .edgesIgnoringSafeArea(.all)
+                VStack(spacing: 0) {
+                    
+                    HStack(spacing: 8) {
+                        Circle()
+                            .fill(pageNum == 1 ? Color.white : Color.white.opacity(0.5))
+                            .frame(width: 8, height: 8)
+                            .cornerRadius(19)
+                        
+                        Circle()
+                            .fill(pageNum == 2 ? Color.white : Color.white.opacity(0.5))
+                            .frame(width: 8, height: 8)
+                            .cornerRadius(19)
+                        
+                        Circle()
+                            .fill(pageNum == 3 ? Color.white : Color.white.opacity(0.5))
+                            .frame(width: 8, height: 8)
+                            .cornerRadius(19)
+                    }
+                    .padding(8).padding(.horizontal, 4)
+                    .background(Color.black.opacity(0.55))
+                    .cornerRadius(50)
+                    VStack {
+                        switch pageNum {
+                        case 1:
+                            VStack(spacing: 12) {
+                                Text("Monitoring of statistics!")
+                                    .font(.title)
+                                    .fontWeight(.semibold)
+                                    .multilineTextAlignment(.center)
+                                Text("Easily track order counts, revenue, popular items and sales trends.")
+                                    .font(.system(size: 16))
+                                    .multilineTextAlignment(.center)
+                            }.frame(height: 85).padding(.horizontal, 30).foregroundColor(.white).padding(.top, 20).padding(.bottom, 40)
+                        case 2:
+                            VStack(spacing: 12) {
+                                Text("Order management")
+                                    .font(.title)
+                                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                                    .multilineTextAlignment(.center)
+                                Text("Quickly add new orders, update information and track their status.")
+                                    .font(.system(size: 15))
+                                    .multilineTextAlignment(.center)
+                            }.frame(height: 85).padding(.horizontal, 30).foregroundColor(.white).padding(.top, 20).padding(.bottom, 40)
+                        case 3:
+                            VStack(spacing: 12) {
+                                Text("Run your business easily!")
+                                    .font(.title)
+                                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                                    .multilineTextAlignment(.center)
+                                Text("The user-friendly interface will help you focus on creating sweet masterpieces.")
+                                    .font(.system(size: 15))
+                                    .multilineTextAlignment(.center)
+                            }.frame(height: 85).padding(.horizontal, 30).foregroundColor(.white).padding(.top, 20).padding(.bottom, 40)
+                        default:
+                            Text("Save information about \nyour favorite routes")
+                                .font(.title)
+                                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                                .frame(height: 70)
+                                .multilineTextAlignment(.center)
+                                .foregroundColor(.white)
+                                .padding(.bottom, 10)
+                            
+                        }
+                        
+                    }
+                    switch pageNum {
+                    case 1: Image("app926Screen1")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: UIScreen.main.bounds.width)
+                        
+                    case 2: Image("app926Screen2")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: UIScreen.main.bounds.width)
+                    case 3: Image("app926Screen3")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: UIScreen.main.bounds.width)
+                    default:
+                        Image("appScreen3")
+                            .resizable()
+                            .frame(height: 549)
+                            .ignoresSafeArea()
+                    }
+                    
+                    ZStack {
+                        Rectangle()
+                            .foregroundColor(.mainRed)
+                            .cornerRadius(30)
+                        
+                        Button {
+                            if pageNum < 3 {
+                                pageNum += 1
+                            } else {
+                                signedUP = true
+                            }
+                        } label: {
+                            ZStack {
+                                Rectangle()
+                                    .frame(height: 60)
+                                    .foregroundColor(.mainRed)
+                                        .cornerRadius(35)
+                                Text("Next")
+                                    .foregroundColor(Color.white)
+                                    .font(.system(size: 20, weight: .semibold))
+                                    .padding()
+                                    .frame(maxWidth: .infinity)
+                            }
+                        }
+                        
+                    }
+                    
+                    
+                }.edgesIgnoringSafeArea(.bottom)
+            }
+            
+        } else {
+          //  TabUIView()
+        }
     }
 }
+
 
 #Preview {
     ReOnboardingUIView()
--- app926/app926/Loader/LoaderUIView.swift
+++ app926/app926/Loader/LoaderUIView.swift
@@ -8,8 +8,75 @@
 import SwiftUI
 
 struct LoaderUIView: View {
+    @State private var progress: Double = 0.0
+    @State private var timer: Timer?
+    @State private var isLoadingView: Bool = true
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        if isLoadingView {
+            ZStack {
+                Color.loaderBG
+                    .ignoresSafeArea()
+                
+                VStack {
+                    Spacer()
+                    Image("logo926")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: UIScreen.main.bounds.width - 200)
+                        
+                        
+                    
+                    Spacer()
+                    Spacer()
+                    ZStack {
+                        ProgressView()
+                            .progressViewStyle(CircularProgressViewStyle(tint: .mainRed))
+                            .scaleEffect(x: 1.7, y: 1.7, anchor: .center)
+                            
+                            
+                    }
+                    .foregroundColor(.black)
+                    .padding(14)
+                    .padding(.bottom, 60)
+                }
+                .onAppear {
+                    startTimer()
+                }
+                .onDisappear {
+                    timer?.invalidate()
+                }
+                
+            }
+            
+        } else {
+            if true {
+                ReOnboardingUIView()
+            } else {
+                Text("US")
+            }
+//            if isWithinTwoDays() {
+//                ReOnboardingUIView()
+//
+//            } else if getAccess() == false {
+//                UsOnboardingUIView()
+//            } else {
+//                ReOnboardingUIView()
+//            }
+            
+        }
+    }
+    func startTimer() {
+        timer?.invalidate()
+        progress = 0
+        timer = Timer.scheduledTimer(withTimeInterval: 0.07, repeats: true) { timer in
+            if progress < 100 {
+                progress += 1
+            } else {
+                timer.invalidate()
+                isLoadingView.toggle()
+            }
+        }
     }
 }
 
