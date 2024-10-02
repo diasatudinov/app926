e4075c3b96aee71da5fdbe849b3372c5e9e02924
Dias <dias.atudinov@gmail.com>
2024-10-02 15:54:32 +0500

    New files

---
+++ app926/app926/Loader/LoaderUIView.swift
@@ -0,0 +1,18 @@
+//
+//  LoaderUIView.swift
+//  app926
+//
+//  Created by Dias Atudinov on 02.10.2024.
+//
+
+import SwiftUI
+
+struct LoaderUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoaderUIView()
+}
---
+++ app926/app926/app926App.swift
@@ -0,0 +1,17 @@
+//
+//  app926App.swift
+//  app926
+//
+//  Created by Dias Atudinov on 02.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct app926App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
