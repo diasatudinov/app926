f38728686cf1a2b0dce175857187e5e571315ccc
Dias <dias.atudinov@gmail.com>
2024-10-04 18:05:05 +0500

    Home view model

--- app926/app926/First/HomeView/ViewModel/HomeViewModel.swift
+++ app926/app926/First/HomeView/ViewModel/HomeViewModel.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+
+class HomeViewModel: ObservableObject {
+    
+    @Published var order: Order = Order(fulfilled: 0, total: 0, earnings: 0) {
+        didSet {
+            progressCount()
+        }
+    }
+    @Published var progress = 0.0
+    
+    func progressCount() {
+        var progress = 0.0
+        progress = Double(order.fulfilled)/Double(order.total)
+        if progress == 0 {
+            self.progress = 0
+        } else {
+            
+            self.progress = progress
+        }
+    }
+    
+    func editOrder(_ newFulfilled: Int, _ newTotal: Int) {
+        order.fulfilled = newFulfilled
+        order.total = newTotal
+    }
+    
+    func editEarning(_ newEarning: Double) {
+        order.earnings = newEarning
+    }
+    
+}
